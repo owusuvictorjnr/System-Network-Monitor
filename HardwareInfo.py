e126810be09c9884e34ec79831420c5e03fbd5ea
owusu victor <owusuvictorjnr>
2022-07-10 00:29:23 -0400

    add GPU and Disk usage

--- HardwareInfo.py
+++ HardwareInfo.py
@@ -1,10 +1,19 @@
 from os import uname
+from pendulum import time
 import psutil
 import platform
 from datetime import datetime
-
+import GPUtil
+from tabulate import tabulate
+import time 
 from sympy import factor
 
+
+
+# me = f"Vitech Solutions"
+
+# print(me, "\n\n\n")
+# time.sleep(5)
 
 
 # a function that converts a large number of bytes into a scaled format 
@@ -74,3 +83,138 @@ print("CPU Usage Per Core")
 for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
     print(f"Core {i}: {percentage}%")
 print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+
+
+
+
+#Memory Usage
+#Memory Information
+print("="*40, "Memory Information", "="*40)
+
+
+#Get the memory details
+svmem = psutil.virtual_memory()
+
+print(f"Total: {get__size(svmem.total)}")
+print(f"Available: {get__size(svmem.available)}")
+print(f"Used: {get__size(svmem.used)}")
+print(f"Percentage: {get__size(svmem.percent)}")
+
+print("="*20, "SWAP", "="*20)
+
+#get the swap memory details(if exists)
+swap = psutil.swap_memory()
+
+print(f"Total: {get__size(swap.total)}")
+print(f"Free: {get__size(swap.free)}")
+print(f"Used: {get__size(swap.used)}")
+print(f"Percentage: {get__size(swap.percent)}")
+
+
+
+
+# Disk Usage
+print("="*40, "Disk Information", "="*40)
+
+print("Partitions and Usage:")
+
+#Get all disk partitions 
+partitions = psutil.disk_partitions()
+for partition in partitions:
+    print(f"=== Devices: {partition.device} ===")
+    print(f"    Mountpoint: {partition.mountpoint}")
+    print(f"    File system type: {partition.fstype }")
+    
+    try:
+        partition_usage = psutil.disk_usage(partition.mountpoint)
+        
+    except PermissionError:
+        #This can be catched due to the disk that is not ready
+        continue
+    
+    print(f"    Total Size: {get__size(partition_usage.total)}")
+    print(f"    Used: {get__size(partition_usage.used)}")
+    print(f"    Free: {get__size(partition_usage.free)}")
+    print(f"    Percentage: {get__size(partition_usage.percent)}")
+    
+    
+    
+#Get IO stats since boot
+disk_io = psutil.disk_io_counters()
+print(f"Total read: {get__size(disk_io.read_bytes)}")
+print(f"Total write: {get__size(disk_io.write_bytes)}\n")
+
+
+
+# Network Information
+
+print("="*40, "Network Information", "="*40)
+
+#Get all network interfaces (virtual and Physical)
+if_addrs = psutil.net_if_addrs()
+for interface_name, interface_addresses in if_addrs.items():
+    for address in interface_addresses:
+        print(f"=== Interface: {interface_name}===")
+        if str(address.family) == 'AddressFamily.AF_INET':
+            print(f"    IP Address: {address.address}")
+            print(f"    Netmask: {address.netmask}")
+            print(f"    Broadcast IP: {address.broadcast}")
+            
+        elif str (address.family) == 'AddressFamily.AF_PACKET':
+            print(f"    MAC Address: {address.address}")
+            print(f"    Netmask: {address.netmask}")
+            print(f"    Broadcast MAC: {address.broadcast}")
+            
+            
+                        
+#Get IO stats since boot
+net_io = psutil.net_io_counters()
+print(f"Total Byte Sent: {get__size(net_io.bytes_sent)}")
+print(f"Total Byte Received: {get__size(net_io.bytes_recv)}")
+
+
+
+
+# GPU Information
+print("="*40, "GPU Details", "="*40)
+
+
+gpus = GPUtil.getGPUs()
+list_gpus = []
+
+for gpu in gpus:
+    #Get the GPU id 
+    gpu_id = gpu.id
+    
+    #Name of GPU 
+    gpu_name = gpu.name
+    
+    #Get % percentage of GPU usage of that GPU
+    gpu_load = f"{gpu.load*100}%"
+    
+    #Get free memory in MB format
+    gpu_free_memory = f"{gpu.memoryFree}MB"
+    
+    #get used memory
+    gpu_used_memory = f"{gpu.memoryUsed}MB"
+    
+    # get total memory
+    gpu_total_memory = f"{gpu.memoryTotal}MB"
+    
+    # get GPU temperature in Celsius
+    gpu_temperature = f"{gpu.temperature} °C"
+    gpu_uuid = gpu.uuid
+    list_gpus.append((
+        gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
+        gpu_total_memory, gpu_temperature, gpu_uuid
+    ))
+    
+    
+print(tabulate(list_gpus, headers=("id", "name", "load", "free memory", "used memory", "total memory",
+                                   "temperature", "uuid")), "\n\n\n")
+
+
+
+
+
+print(f"credit to Abdou Rockikz ")
